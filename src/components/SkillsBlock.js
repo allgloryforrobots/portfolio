23c0e43f1bee2b1a84c324cf8a8ee09794343d00
user <frontendreactjs@gmail.com>
2022-11-30 08:54:11 +0300

    'skills_layout'

---
+++ src/components/SkillsBlock.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const SkillsBlock= () => {
+    return (
+        <div className='skill-block'>
+            <div className='skill-block__card'>1</div>
+            <div className='skill-block__card'>1</div>
+        </div>
+    )
+}
--- src/pages/SkillsPage.js
+++ src/pages/SkillsPage.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { SkillsBlock } from '../components/SkillsBlock'
 
 export const SkillsPage = () => {
     return (
         <div className="wrapper wrapper--skills">
-            Skills
+            <SkillsBlock/>
         </div>
     )
 }
