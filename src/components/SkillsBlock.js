4a2154adcd8265feadb345534c79c042b31d8177
user <frontendreactjs@gmail.com>
2022-12-02 09:02:21 +0300

    'skills_cards'

--- src/components/SkillsBlock.js
+++ src/components/SkillsBlock.js
@@ -3,8 +3,75 @@ import React from 'react'
 export const SkillsBlock= () => {
     return (
         <div className='skill-block'>
-            <div className='skill-block__card'>1</div>
-            <div className='skill-block__card'>1</div>
+            <div className='skill-block__card'>
+                
+                <div className='skill-block__header'>
+                    Current project
+                </div>
+
+                <div className='skill-block__section'>
+                    <div className='skill-block__title'>☀️  Frontend:</div>
+                    <div className='skill-block__row'>JavaScript (ES6+)</div>
+                    <div className='skill-block__row'>Pure JS OOP</div>
+                    <div className='skill-block__row'>SCSS</div>
+                    <div className='skill-block__row'>Semantic UI</div>
+                    <div className='skill-block__row'>Gulp / Rollup</div>
+                    <div className='skill-block__row'></div>
+                    <div className='skill-block__row'></div>
+                    <div className='skill-block__row'></div>
+                </div>
+
+                <div className='skill-block__section'>
+                    <div className='skill-block__title'>🌙  Backend:</div>
+                    <div className='skill-block__row'>Node.JS / Express </div>
+                    <div className='skill-block__row'>WebSocket</div>
+                </div>
+
+                <div className='skill-block__section'>
+                    <div className='skill-block__title'>About:</div>
+                    <div className='skill-block__row'>Node.JS / Express </div>
+                </div>
+
+            </div>
+
+            <div className='skill-block__card'>
+
+                <div className='skill-block__header'>
+                    What can I do
+                </div>
+
+                <div className='skill-block__section'>
+                    <div className='skill-block__title'>☀️  Frontend:</div>
+                    <div className='skill-block__row'>JavaScript (ES6+)</div>
+                    <div className='skill-block__row'>Pure JS OOP / React</div>
+                    <div className='skill-block__row'>Redux Toolkit / Redux / Redux Thunk</div>
+                    <div className='skill-block__row'>React Hook Form</div>
+                    <div className='skill-block__row'>SCSS / styled-components / CSS Modules </div>
+                    <div className='skill-block__row'>Material UI / Semantic UI / Ant Design</div>
+                    <div className='skill-block__row'>Gulp / Rollup / Webpack</div>
+                    <div className='skill-block__row'></div>
+                    <div className='skill-block__row'></div>
+                    <div className='skill-block__row'></div>
+                </div>
+
+                <div className='skill-block__section'>
+                    <div className='skill-block__title'>🌙  Backend:</div>
+                    <div className='skill-block__row'>Node.JS / Express </div>
+                    <div className='skill-block__row'>MongoDB / Mongoose</div>
+                    <div className='skill-block__row'>JWT </div>
+                    <div className='skill-block__row'>WebSocket</div>
+                </div>
+                
+                <div className='skill-block__section'>
+                    <div className='skill-block__title'>Other skills:</div>
+                    <div className='skill-block__row'>Git / GitHub </div>
+                    <div className='skill-block__row'>SOLID</div>
+                    <div className='skill-block__row'>Docker </div>
+                    <div className='skill-block__row'>Redmine</div>
+                    <div className='skill-block__row'>BEM (CSS)</div>
+                </div>
+
+            </div>
         </div>
     )
 }
