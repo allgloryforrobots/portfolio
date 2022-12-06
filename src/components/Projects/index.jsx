af8b0484fae7666d6a0026eb9ac18d50219f0f4a
FrontendReactJS <allgloryforrobots@gmail.com>
2022-12-06 08:06:15 +0300

    'grid_layout'

---
+++ src/components/Projects/index.jsx
@@ -0,0 +1,29 @@
+import styles from './Projects.module.scss'
+
+
+export const Projects = () => {
+
+    return (
+        <div className={styles.projects}>
+            <div className={styles.projects__card}>
+                1
+            </div>
+            <div className={styles.projects__card}>
+                1
+            </div>
+            <div className={styles.projects__card}>
+                1
+            </div>
+            <div className={styles.projects__card}>
+                1
+            </div>
+            <div className={styles.projects__card}>
+                1
+            </div>
+            <div className={styles.projects__card}>
+                1
+            </div>
+        </div>
+    )
+
+}
--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Projects } from '../components/Projects'
 
 export const ProjectsPage = () => {
     return (
         <div className="wrapper wrapper--projects">
-            Projects
+            <Projects/>
         </div>
     )
 }
